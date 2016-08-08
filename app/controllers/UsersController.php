effeb60fc89c8a6fa39f9e6f2ef2fb7fdd39dd87
evanson nguku n <ngukuevans@gmail.com>
2016-08-08 11:02:44 +0300

    Add files via upload

---
+++ app/controllers/UsersController.php
@@ -0,0 +1,241 @@
+<?php
+ 
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+
+
+class UsersController extends ControllerBase
+{
+    /**
+     * Index action
+     */
+    public function indexAction()
+    {
+        $this->persistent->parameters = null;
+    }
+
+    /**
+     * Searches for users
+     */
+    public function searchAction()
+    {
+        $numberPage = 1;
+        if ($this->request->isPost()) {
+            $query = Criteria::fromInput($this->di, 'Users', $_POST);
+            $this->persistent->parameters = $query->getParams();
+        } else {
+            $numberPage = $this->request->getQuery("page", "int");
+        }
+
+        $parameters = $this->persistent->parameters;
+        if (!is_array($parameters)) {
+            $parameters = [];
+        }
+        $parameters["order"] = "userID";
+
+        $users = Users::find($parameters);
+        if (count($users) == 0) {
+            $this->flash->notice("The search did not find any users");
+
+            $this->dispatcher->forward([
+                "controller" => "users",
+                "action" => "index"
+            ]);
+
+            return;
+        }
+
+        $paginator = new Paginator([
+            'data' => $users,
+            'limit'=> 10,
+            'page' => $numberPage
+        ]);
+
+        $this->view->page = $paginator->getPaginate();
+    }
+
+    /**
+     * Displays the creation form
+     */
+    public function newAction()
+    {
+
+    }
+
+    /**
+     * Edits a user
+     *
+     * @param string $userID
+     */
+    public function editAction($userID)
+    {
+        if (!$this->request->isPost()) {
+
+            $user = Users::findFirstByuserID($userID);
+            if (!$user) {
+                $this->flash->error("user was not found");
+
+                $this->dispatcher->forward([
+                    'controller' => "users",
+                    'action' => 'index'
+                ]);
+
+                return;
+            }
+
+            $this->view->userID = $user->userID;
+
+            $this->tag->setDefault("userID", $user->userID);
+            $this->tag->setDefault("Name", $user->Name);
+            $this->tag->setDefault("Email", $user->Email);
+            $this->tag->setDefault("Password", $user->Password);
+            $this->tag->setDefault("Orders", $user->Orders);
+            
+        }
+    }
+
+    /**
+     * Creates a new user
+     */
+    public function createAction()
+    {
+        if (!$this->request->isPost()) {
+            $this->dispatcher->forward([
+                'controller' => "users",
+                'action' => 'index'
+            ]);
+
+            return;
+        }
+
+        $user = new Users();
+        $user->userID = $this->request->getPost("userID");
+        $user->Name = $this->request->getPost("Name");
+        $user->Email = $this->request->getPost("Email");
+        $user->Password = $this->request->getPost("Password");
+        $user->Orders = $this->request->getPost("Orders");
+        
+
+        if (!$user->save()) {
+            foreach ($user->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            $this->dispatcher->forward([
+                'controller' => "users",
+                'action' => 'new'
+            ]);
+
+            return;
+        }
+
+        $this->flash->success("user was created successfully");
+
+        $this->dispatcher->forward([
+            'controller' => "users",
+            'action' => 'index'
+        ]);
+    }
+
+    /**
+     * Saves a user edited
+     *
+     */
+    public function saveAction()
+    {
+
+        if (!$this->request->isPost()) {
+            $this->dispatcher->forward([
+                'controller' => "users",
+                'action' => 'index'
+            ]);
+
+            return;
+        }
+
+        $userID = $this->request->getPost("userID");
+        $user = Users::findFirstByuserID($userID);
+
+        if (!$user) {
+            $this->flash->error("user does not exist " . $userID);
+
+            $this->dispatcher->forward([
+                'controller' => "users",
+                'action' => 'index'
+            ]);
+
+            return;
+        }
+
+        $user->userID = $this->request->getPost("userID");
+        $user->Name = $this->request->getPost("Name");
+        $user->Email = $this->request->getPost("Email");
+        $user->Password = $this->request->getPost("Password");
+        $user->Orders = $this->request->getPost("Orders");
+        
+
+        if (!$user->save()) {
+
+            foreach ($user->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            $this->dispatcher->forward([
+                'controller' => "users",
+                'action' => 'edit',
+                'params' => [$user->userID]
+            ]);
+
+            return;
+        }
+
+        $this->flash->success("user was updated successfully");
+
+        $this->dispatcher->forward([
+            'controller' => "users",
+            'action' => 'index'
+        ]);
+    }
+
+    /**
+     * Deletes a user
+     *
+     * @param string $userID
+     */
+    public function deleteAction($userID)
+    {
+        $user = Users::findFirstByuserID($userID);
+        if (!$user) {
+            $this->flash->error("user was not found");
+
+            $this->dispatcher->forward([
+                'controller' => "users",
+                'action' => 'index'
+            ]);
+
+            return;
+        }
+
+        if (!$user->delete()) {
+
+            foreach ($user->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            $this->dispatcher->forward([
+                'controller' => "users",
+                'action' => 'search'
+            ]);
+
+            return;
+        }
+
+        $this->flash->success("user was deleted successfully");
+
+        $this->dispatcher->forward([
+            'controller' => "users",
+            'action' => "index"
+        ]);
+    }
+
+}
---
+++ app/models/Users.php
@@ -0,0 +1,73 @@
+<?php
+
+class Users extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     * @Column(type="integer", length=10, nullable=false)
+     */
+    public $userID;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    public $Name;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", length=70, nullable=false)
+     */
+    public $Email;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", length=10, nullable=false)
+     */
+    public $Password;
+
+    /**
+     *
+     * @var integer
+     * @Column(type="integer", length=11, nullable=false)
+     */
+    public $Orders;
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'users';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
